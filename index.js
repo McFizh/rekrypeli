9870b1b8d4c440fa3487fa7db9d0d74b5fe2f3be
Pekka Harjamäki <spamtrap_github@mcfish.org>
2018-10-12 10:54:27 +0300

    Added dockerfile and mongo connectivity

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const Hapi = require('hapi');
 const Good = require('good');
 
+const mongo = require('./lib/database');
+const routes = require('./lib/routes');
+
 require('dotenv').config();
 
 // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -10,10 +13,16 @@ require('dotenv').config();
 // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 const init = async function() {
 
-    //
+    // Connect to database
+    await mongo.connect();
+
+    // Initialize hapi server
     const server = new Hapi.Server({
         port: 8080
     });
+
+    // Register routes
+    routes.register(server);
 
     // Start the server
     await server.start();
